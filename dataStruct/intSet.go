a8148093ba6930e6ac59d74afac0a28fd553f58f
xianyunyh <tianlei542475736@gmail.com>
2019-12-14 00:09:53 +0800

    补充inset

--- dataStruct/intSet.go
+++ dataStruct/intSet.go
@@ -12,45 +12,53 @@ func NewIntSet() *IntSet {
 	}
 }
 
-func (self *IntSet) Insert(value int64) bool {
-	var pos int = 0
-	if self.Search(value, &pos) {
+func (is *IntSet) Insert(value int64) bool {
+	var pos int = -1
+	if is.Search(value, &pos) {
 		return false
 	}
-	return false
+	if pos < 0 {
+		return false
+	}
+	is.MoveTail(pos, is.lens+1)
+	is.items[pos] = value
+	is.lens++
+	return true
 }
 
-func (self *IntSet) Find(pos int) int64 {
-
-	return 0
+func (is *IntSet) Find(pos int) int64 {
+	if pos < 0 {
+		return -1
+	}
+	return  is.items[pos]
 }
 
-func (self *IntSet) Search(value int64, pos *int) bool {
+func (is *IntSet) Search(value int64, pos *int) bool {
 	var setPos = func(idx int, pos *int) {
 		if pos != nil {
 			*pos = idx
 		}
 	}
 	//长度为0
-	if self.lens == 0 {
+	if is.lens == 0 {
 		setPos(0, pos)
 		return false
 	} else {
 		//不在范围内
-		if value < self.items[0] {
+		if value < is.items[0] {
 			setPos(0, pos)
 			return false
-		} else if value > self.items[self.lens-1] {
-			setPos(self.lens, pos)
+		} else if value > is.items[is.lens-1] {
+			setPos(is.lens, pos)
 			return false
 		}
 	}
-	max, min := 0, self.lens-1
+	max, min := 0, is.lens-1
 	var mid int = -1
 	var cur int64 = -1
 	for max > min {
-		mid = int((max + min) >> 1)
-		cur = self.Find(mid)
+		mid = (max + min) >> 1
+		cur = is.Find(mid)
 		if cur > value {
 			min = mid + 1
 		} else if cur < value {
@@ -68,14 +76,22 @@ func (self *IntSet) Search(value int64, pos *int) bool {
 
 }
 
-func (self *IntSet) MoveTail(from, to int) {
-
+func (is *IntSet) MoveTail(from, to int) {
+	is.items = append(is.items,0)
+	for to > from {
+		is.items[to] = is.items[to-1]
+		to--
+	}
 }
 
-func (self *IntSet) Remove(value int64) bool {
-
+func (is *IntSet) Remove(value int64) bool {
+	var pos int  = -1
+	if !is.Search(value,&pos) {
+		return false
+	}
+	is.items = append(is.items[:pos], is.items[pos:]...)
 	return true
 }
-func (self *IntSet) Lens() int {
-	return self.lens
+func (is *IntSet) Lens() int {
+	return is.lens
 }
