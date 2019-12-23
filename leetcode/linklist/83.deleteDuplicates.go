431affcf0646eb84ed2c7b7cf556137320fa6d08
xianyunyh <tianlei542475736@gmail.com>
2019-12-23 23:53:10 +0800

    增加删除链表重复数据

---
+++ leetcode/linklist/83.deleteDuplicates.go
@@ -0,0 +1,23 @@
+package linklist
+
+// ListNode 链表结点
+type ListNode struct {
+	Next *ListNode
+	Val  int
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	if current == nil || current.Next == nil {
+		return head
+	}
+	for current != nil && current.Next != nil {
+		next := current.Next
+		if current.Val == next.Val {
+			current.Next = next.Next
+		} else {
+			current = current.Next
+		}
+	}
+	return head
+}
---
+++ leetcode/linklist/link_test.go
@@ -0,0 +1,7 @@
+package linklist
+
+import "testing"
+
+func TestDeleteDuplicates(t *testing.T) {
+	// a := assert.New(t)
+}
