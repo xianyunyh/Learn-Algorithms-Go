d8afbeca7d14019e5bb5aa0c87f7dc02a9fe43f1
xianyunyh <tianlei542475736@gmail.com>
2019-12-24 19:36:47 +0800

    add leetcode strStr

--- leetcode/string/strStr.go
+++ leetcode/string/strStr.go
@@ -1,26 +1,35 @@
 package string
 
 // TODO
+// 思路
+// 用子串的第一个第一个字符串进行
 func strStr(haystack string, needle string) int {
-	if needle == "" {
+	//return strings.Index(haystack, needle)
+	strLen := len(haystack)
+	subStrLen := len(needle)
+	if subStrLen == 0 {
 		return 0
+	}
+	if strLen < subStrLen {
+		return -1
 	}
 	i := 0
 	j := 0
-	char := needle[i]
-	for ; j < len(haystack); j++ {
-		if haystack[j] != char {
+	for ; j < strLen; j++ {
+		if haystack[j] != needle[i] {
+			if i > 0 {
+				j = j - i
+				i = 0
+			}
 			continue
 		}
 		i++
 		if i == len(needle) {
 			break
 		}
-		char = needle[i]
 	}
 	if i == len(needle) {
 		return j - len(needle) + 1
 	}
 	return -1
-
 }
--- leetcode/string/string_test.go
+++ leetcode/string/string_test.go
@@ -1,14 +1,16 @@
 package string
 
-import "testing"
-
-import "log"
+import (
+	"log"
+	"strings"
+	"testing"
+)
 
 func TestLongestCommonPrefix(t *testing.T) {
 	strs := []string{"flower", "flow", "flight"}
 	s := longestCommonPrefix(strs)
 	log.Println(s)
-
-	i := strStr("hello", "ll")
+	i := strStr("mississippi", "issip")
 	log.Println(i)
+	log.Println(strings.Index("mississippi", "issip"))
 }
