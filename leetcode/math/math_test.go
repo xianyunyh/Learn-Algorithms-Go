6ecef31daf380e4419760d68ba4f9e9cc3b63ead
xianyunyh <tianlei542475736@gmail.com>
2019-12-25 18:08:00 +0800

    add 67.二进制求和

--- leetcode/math/math_test.go
+++ leetcode/math/math_test.go
@@ -4,6 +4,8 @@ import "testing"
 
 import "github.com/stretchr/testify/assert"
 
+import "log"
+
 func TestReverse(t *testing.T) {
 	reverse(1534236469)
 }
@@ -14,5 +16,7 @@ func TestRomanToInt(t *testing.T) {
 	a.Equal(romanToInt("III"), 3)
 	a.Equal(romanToInt("IV"), 4)
 	a.Equal(romanToInt("LVIII"), 58)
+	s := addBinary("11", "1")
+	log.Println(s)
 
 }
