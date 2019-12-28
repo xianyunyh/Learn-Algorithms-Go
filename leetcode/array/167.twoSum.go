e39149ea011794fe68cefb344e2ca918d224397b
xianyunyh <tianlei542475736@gmail.com>
2019-12-28 23:47:35 +0800

    add 167.twosum

---
+++ leetcode/array/167.twoSum.go
@@ -0,0 +1,19 @@
+package array
+
+func twoSum(numbers []int, target int) []int {
+
+	max := len(numbers) - 1
+	min := 0
+	for min < max {
+		sum := numbers[min] + numbers[max]
+		if sum == target {
+			return []int{min + 1, max + 1}
+		}
+		if sum > target {
+			max--
+		} else {
+			min++
+		}
+	}
+	return []int{-1, -1}
+}
