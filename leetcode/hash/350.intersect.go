c28186a25fb23f26b069b85b8bc0d677e843f51e
xianyunyh <tianlei542475736@gmail.com>
2020-01-14 16:20:55 +0800

    add leetcode 350

---
+++ leetcode/hash/350.intersect.go
@@ -0,0 +1,21 @@
+package hash
+
+// 解题思路 利用每个hash 计数
+func intersect(nums1 []int, nums2 []int) []int {
+	table := make(map[int]int)
+	for _, v := range nums1 {
+		if _, ok := table[v]; ok {
+			table[v] += 1
+		} else {
+			table[v] = 1
+		}
+	}
+	result := []int{}
+	for _, v := range nums2 {
+		if _, ok := table[v]; ok && table[v] > 0 {
+			result = append(result, v)
+			table[v]--
+		}
+	}
+	return result
+}
