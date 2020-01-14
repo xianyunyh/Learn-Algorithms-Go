fe147c31ec2dcc3c0b2474d7f5d1b2f963aa0e0e
xianyunyh <tianlei542475736@gmail.com>
2020-01-14 16:01:45 +0800

    add leetcode 290

---
+++ leetcode/hash/290.wordPattern.go
@@ -0,0 +1,30 @@
+package hash
+
+import "strings"
+
+func wordPattern(pattern string, str string) bool {
+	strs := strings.Split(str, " ")
+	if len(strs) != len(pattern) {
+		return false
+	}
+	table := make(map[rune]string)
+	set := make(map[string]rune)
+
+	for k, v := range pattern {
+		if _, ok := table[v]; !ok {
+			table[v] = strs[k]
+			if _, ok2 := set[strs[k]]; !ok2 {
+				set[strs[k]] = v
+			} else {
+				if set[strs[k]] != v {
+					return false
+				}
+			}
+		} else {
+			if table[v] != strs[k] {
+				return false
+			}
+		}
+	}
+	return true
+}
