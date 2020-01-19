8b1dc2a9115f0705adaafa5e79e7c8cbbad16c93
xianyunyh <tianlei542475736@gmail.com>
2020-01-19 14:54:22 +0800

    add leetcode 347 topK

---
+++ leetcode/hash/347.topK.go
@@ -0,0 +1,79 @@
+package hash
+
+import "container/heap"
+
+// @lc code=start
+//实现堆
+type Item struct {
+	value    int // The value of the item; arbitrary.
+	priority int // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value int, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+func topKFrequent(nums []int, k int) []int {
+	table := make(map[int]int)
+	for _, v := range nums {
+		if _, ok := table[v]; ok {
+			table[v]++
+		} else {
+			table[v] = 1
+		}
+	}
+	pq := make(PriorityQueue, len(table))
+	i := 0
+	for value, priority := range table {
+		pq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+	heap.Init(&pq)
+	result := make([]int, 0)
+	for i := 0; i < k; i++ {
+		temp := heap.Pop(&pq).(*Item)
+		result = append(result, temp.value)
+	}
+	return result
+}
