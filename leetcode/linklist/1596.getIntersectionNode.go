4c38b46a629d49e75be930e0e91ef433378ecc93
xianyunyh <tianlei542475736@gmail.com>
2020-06-24 23:29:40 +0800

    add leetcode 1596

---
+++ leetcode/linklist/1596.getIntersectionNode.go
@@ -0,0 +1,19 @@
+package linklist
+
+// 同leetcode 160
+func GetIntersectionNode(headA, headB *ListNode) *ListNode {
+	nodeA, nodeB := headA, headB
+	for nodeA != nodeB {
+		if nodeA != nil {
+			nodeA = nodeA.Next
+		} else {
+			nodeA = headB
+		}
+		if nodeB != nil {
+			nodeB = nodeB.Next
+		} else {
+			nodeB = headA
+		}
+	}
+	return nodeA
+}
