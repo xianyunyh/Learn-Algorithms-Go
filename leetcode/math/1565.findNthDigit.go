4ed74bf32b3043702b1008f1c80adc2dc921321b
xianyunyh <tianlei542475736@gmail.com>
2020-07-07 00:23:12 +0800

    add leetcode 1565

--- leetcode/math/1565.findNthDigit.go
+++ leetcode/math/1565.findNthDigit.go
@@ -2,14 +2,15 @@ package math
 
 import "strconv"
 
-func t(n int) int {
+func findNthDigit(n int) int {
 	digit, start, count := 1, 1, 9
 	for n > count {
+		n -= count
 		start = start * 10
 		digit += 1
 		count = 9 * start * digit
 	}
-	num := start + (n - 1)
+	num := start + (n-1)/digit
 	s := strconv.Itoa(num)
-	return int(s[(num-1)%digit] - '0')
+	return int(s[(n-1)%digit] - '0')
 }
