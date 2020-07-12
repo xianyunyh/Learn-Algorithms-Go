d105a03288c1d8d921ed9cd51b1a27559de39022
xianyunyh <tianlei542475736@gmail.com>
2020-07-12 22:00:36 +0800

    leetcode 5469

--- leetcode/array/1502.canMakeArithmeticProgression.go
+++ leetcode/array/1502.canMakeArithmeticProgression.go
@@ -18,21 +18,3 @@ func canMakeArithmeticProgression(arr []int) bool {
 	return true
 
 }
-
-// 利用等差公式 s = (a1 +an) *n /2
-func canMakeArithmeticProgression2(arr []int) bool {
-	lens := len(arr)
-	min, max := arr[0], arr[0]
-	sum := arr[0]
-	for i := 1; i < lens; i++ {
-		if arr[i] <= min {
-			min = arr[i]
-		}
-		if arr[i] >= max {
-			max = arr[i]
-		}
-		sum += arr[i]
-	}
-	return ((min + max) * lens) == 2*sum
-
-}
