236b7d54c1392f6d349e0c534d2845bdca8f2232
xianyunyh <tianlei542475736@gmail.com>
2020-07-23 00:10:05 +0800

    add lcoffer 29

---
+++ leetcode/lcoffer/29.spiralOrder.go
@@ -0,0 +1,45 @@
+package lcoffer
+
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 {
+		return []int{}
+	}
+
+	top := 0
+	bottom := len(matrix) - 1
+	left := 0
+	right := len(matrix[0]) - 1
+	k := 0
+	res := make([]int, (bottom+1)*(right+1))
+
+	for left <= right && top <= bottom {
+		//上边一行
+		for i := left; i <= right; i++ {
+			res[k] = matrix[top][i]
+			k++
+		}
+		top++
+
+		//右边一列
+		for i := top; i <= bottom; i++ {
+			res[k] = matrix[i][right]
+			k++
+		}
+		right--
+
+		//下边一行
+		for i := right; i >= left && top <= bottom; i-- {
+			res[k] = matrix[bottom][i]
+			k++
+		}
+		bottom--
+
+		//左边一列
+		for i := bottom; i >= top && left <= right; i-- {
+			res[k] = matrix[i][left]
+			k++
+		}
+		left++
+	}
+	return res
+}
---
+++ leetcode/lcoffer/lcoffer_test.go
@@ -0,0 +1,10 @@
+package lcoffer
+
+import "testing"
+
+func TestSpiralOrder(t *testing.T) {
+	spiralOrder([][]int{
+		{2, 5, 8},
+		{4, 0, -1},
+	})
+}
