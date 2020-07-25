b01e90df270d4f90ce5b626aaeb4a8dc0778e883
xianyunyh <tianlei542475736@gmail.com>
2020-07-26 01:31:49 +0800

    add lcoffer 32

---
+++ leetcode/lcoffer/32.levelOrder.go
@@ -0,0 +1,86 @@
+package lcoffer
+
+func levelOrder1(root *TreeNode) []int {
+	queue := []*TreeNode{}
+	result := []int{}
+	if root == nil {
+		return result
+	}
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		node := queue[0]
+		result = append(result, node.Val)
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+		queue = queue[1:]
+	}
+	return result
+}
+func levelOrder2(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		list := make([]int, 0)
+		// 为什么要取length？
+		// 记录当前层有多少元素（遍历当前层，再添加下一层）
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			// 出队列
+			level := queue[0]
+			queue = queue[1:]
+			list = append(list, level.Val)
+			if level.Left != nil {
+				queue = append(queue, level.Left)
+			}
+			if level.Right != nil {
+				queue = append(queue, level.Right)
+			}
+		}
+		result = append(result, list)
+	}
+	return result
+}
+
+// 奇数偶数行颠倒
+func levelOrder3(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		list := make([]int, 0)
+		// 为什么要取length？
+		// 记录当前层有多少元素（遍历当前层，再添加下一层）
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			// 出队列
+			level := queue[0]
+			queue = queue[1:]
+			if len(result)%2 == 1 {
+				list = append([]int{level.Val}, list...)
+			} else {
+				list = append(list, level.Val)
+			}
+
+			if level.Left != nil {
+				queue = append(queue, level.Left)
+			}
+			if level.Right != nil {
+				queue = append(queue, level.Right)
+			}
+		}
+		result = append(result, list)
+	}
+	return result
+}
