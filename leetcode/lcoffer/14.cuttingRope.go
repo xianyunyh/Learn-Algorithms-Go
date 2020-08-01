a51f16e30f084309889ef3776f2e8c310de4d1cd
xianyunyh <tianlei542475736@gmail.com>
2020-08-01 16:41:03 +0800

    add lcoffer 14

---
+++ leetcode/lcoffer/14.cuttingRope.go
@@ -0,0 +1,25 @@
+package lcoffer
+
+// 题解 https://leetcode-cn.com/problems/jian-sheng-zi-lcof/solution/mian-shi-ti-14-i-jian-sheng-zi-tan-xin-si-xiang-by/
+func cuttingRope(n int) int {
+	if n <= 3 {
+		return n - 1
+	}
+	a, b := n/3, n%3
+	pow := func(a, b int) int {
+		res := 1
+		for b > 0 {
+			res *= a
+			b--
+		}
+		return res
+	}
+	if b == 0 {
+		return pow(3, a)
+	}
+	if b == 1 {
+		return pow(3, a-1) * 4
+	}
+	return pow(3, a) * 2
+
+}
