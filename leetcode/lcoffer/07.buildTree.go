5f9190e9ca77ee2b2f83148d4c31104067abdbb1
xianyunyh <tianlei542475736@gmail.com>
2020-08-07 17:27:10 +0800

    add lc 07

---
+++ leetcode/lcoffer/07.buildTree.go
@@ -0,0 +1,18 @@
+package lcoffer
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	rootIdx := 0
+	for i := range inorder {
+		if inorder[i] == preorder[0] {
+			rootIdx = i
+			break
+		}
+	}
+	root := &TreeNode{Val: preorder[0]}
+	root.Left = buildTree(preorder[1:rootIdx+1], inorder[:rootIdx])
+	root.Right = buildTree(preorder[rootIdx+1:], inorder[rootIdx+1:])
+	return root
+}
