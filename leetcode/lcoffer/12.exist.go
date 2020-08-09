0b186ab13fc620eabc8293e6bf186b420936ddf6
xianyunyh <tianlei542475736@gmail.com>
2020-08-09 14:31:23 +0800

    add lcoffer 12

---
+++ leetcode/lcoffer/12.exist.go
@@ -0,0 +1,40 @@
+package lcoffer
+
+// 考察dfs深度优先算法，由于可以从四个方向进行遍历
+//  i+1 i-1 j+1 j-1
+func exist(board [][]byte, word string) bool {
+	m, n := len(board), len(board[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if dfs(board, i, j, word, 0) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func dfs(board [][]byte, i int, j int, word string, k int) bool {
+	if board[i][j] != word[k] {
+		return false
+	}
+	if k == len(word)-1 {
+		return true
+	}
+	temp := board[i][j]
+	board[i][j] = byte(' ')
+	if 0 <= i-1 && dfs(board, i-1, j, word, k+1) {
+		return true
+	}
+	if i+1 < len(board) && dfs(board, i+1, j, word, k+1) {
+		return true
+	}
+	if 0 <= j-1 && dfs(board, i, j-1, word, k+1) {
+		return true
+	}
+	if j+1 < len(board[0]) && dfs(board, i, j+1, word, k+1) {
+		return true
+	}
+	board[i][j] = temp
+	return false
+}
