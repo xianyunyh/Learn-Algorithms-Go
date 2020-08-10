12e1378c47471529779668d9d6b694eff7fe0b2b
xianyunyh <tianlei542475736@gmail.com>
2020-08-10 23:21:48 +0800

    add lc 33

---
+++ leetcode/lcoffer/33.verifyPostorder.go
@@ -0,0 +1,17 @@
+package lcoffer
+
+func verifyPostorder(postorder []int) bool {
+	stack := []int{}
+	root := 2 << 31
+	for i := len(postorder) - 1; i >= 0; i-- {
+		if postorder[i] > root {
+			return false
+		}
+		for len(stack) > 0 && stack[len(stack)-1] > postorder[i] {
+			root = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, postorder[i])
+	}
+	return true
+}
