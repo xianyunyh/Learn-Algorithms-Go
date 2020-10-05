a50742ab1ce8d3aea4b7b8845941f3b8e55fe2f9
xianyunyh <tianlei542475736@gmail.com>
2020-10-05 14:37:05 +0800

    add lettcode 724

---
+++ leetcode/array/724.pivotIndex.go
@@ -0,0 +1,19 @@
+package array
+
+func pivotIndex(nums []int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	sum2 := 0
+	for i := 0; i < len(nums); i++ {
+		if sum-nums[i]-sum2 == sum2 {
+			return i
+		}
+		sum2 += nums[i]
+	}
+	return -1
+}
